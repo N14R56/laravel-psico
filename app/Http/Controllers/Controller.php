8401b2b6c9cba03a024d0f6f83a429dc9fd5867a
Matheus Murray <2.0turbocoder@gmail.com>
2022-10-15 16:16:50 -0300

    feat(route): /bem-vindos prototype

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function bem_vindos(Request $request): Response
+    {
+        return response(
+            [
+                'bem-vindos' => 'Essa e a api-web responsavel pelo backend da aplicacao mobile de Desenvolvimento Web.'
+            ]
+        );
+    }
 }
