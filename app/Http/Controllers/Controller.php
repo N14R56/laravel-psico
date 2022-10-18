3d1b1041f23b734dd4e94edb02ab6de8cb665c03
Matheus Murray <2.0turbocoder@gmail.com>
2022-10-17 23:23:36 -0300

    feat: explore domain, /bem-estar, /pergunta, /qrcode, /dica

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Client\Request as ClientRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Http;
 
 class Controller extends BaseController
 {
@@ -17,8 +19,82 @@ class Controller extends BaseController
     {
         return response(
             [
-                'bem-vindos' => 'Essa e a api-web responsavel pelo backend da aplicacao mobile de Desenvolvimento Web.'
-            ]
+                'bem-vindos' => 'Essa é a api-web responsável pelo backend da aplicação mobile de Desenvolvimento Mobile.'
+            ],
+            200
+        );
+    }
+
+    public function pergunta(Request $request): Response
+    {
+        if (null !== $request->get('funcionario_id')) {
+
+            $status = 201;
+
+        } else {
+
+            $status = 200;
+
+        }
+
+        return response(
+            [
+                'funcionario_id' => $request->get('funcionario_id'),
+                'pergunta' => 'Como você está se sentindo hoje?'
+            ],
+            $status
+        );
+    }
+
+    public function bemEstar(Request $request): Response
+    {
+        return response(
+            ["dica" => "Tire um day-off, o seu gestor já foi informado."] ,
+            200
+        );
+    }
+
+    public function qrcode(Request $request): Response
+    {
+        $url = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=funcionario1';
+
+        if (null !== $request->post('funcionario_id')) {
+            
+            $status = 200;
+
+        } else {
+            
+            $status = 201;
+
+        }
+
+        return response(
+            ['url' => $url],
+            $status
+        );
+    }
+
+    public function dica(Request $request): Response
+    {
+        if (null !== $request->post('funcionario-id')) {
+
+            $body = '';
+
+            $status = 200;
+
+        } else {
+            
+            $body = [
+                'dica' => 'Tire um day-off, o seu gestor já foi informado.'
+            ];
+
+            $status = 200;
+
+        }
+
+        return response(
+            $body,
+            $status
         );
     }
 }
---
+++ tests/Feature/AppTest.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+use function PHPUnit\Framework\assertContains;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertIsNotInt;
+use function PHPUnit\Framework\assertIsString;
+use function PHPUnit\Framework\assertNull;
+use function PHPUnit\Framework\assertSame;
+use function PHPUnit\Framework\isNull;
+
+class AppTest extends TestCase
+{
+    /**
+     * GET /bem-vindos
+     * {'bem-vindos' : 'Mensagem de boas-vindas'}
+     * 
+     * 200
+     */
+    public function test_0(): void
+    {
+        $url = '/bem-vindos';
+
+        $response = $this->get($url);
+
+        assertIsString($response->json()['bem-vindos']);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Resgata a pergunta a ser feita ao funcionario.
+     * Funcionario existe.
+     * 
+     * GET /pergunta?funcionario_id=1
+     *  {
+     *      "funcionario_id":"1",
+     *      "pergunta":"Como voc\u00ea est\u00e1 se sentindo hoje?"
+     *  }
+     * 
+     *  201
+     */
+    public function test_1(): void
+    {
+        $url = '/pergunta?funcionario_id=1';
+
+        $response = $this->get($url);
+
+        assertIsString($response->json()['funcionario_id']);
+        assertIsString($response->json()['pergunta']);
+
+        $response->assertStatus(201);
+    }
+
+    /**
+     * Resgata a pergunta a ser feita ao funcionario.
+     * Funcionario nao existe, retorna uma pergunta default.
+     * 
+     * GET /pergunta?funcionario_id=dsadasda
+     *  {
+     *      "funcionario_id":null,
+     *      "pergunta":"Como voc\u00ea est\u00e1 se sentindo hoje?"
+     *  }
+     * 
+     *  200
+     */
+    public function test_2(): void
+    {
+        $url = '/pergunta?funcionario_id=';
+
+        $response = $this->get($url);
+
+        assertNull($response->json()['funcionario_id']);
+
+        assertIsString($response->json()['pergunta']);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Salva a resposta do usuario a barra de bem-estar
+     * 
+     * POST /bem-estar
+     *  {
+     *      "funcionario_id" : 1,
+     *      "bem-estar" : 1
+     *  }
+     * 
+     *  200
+     *  {
+     *      "dica" : "Tire um day-off, o seu gestor já foi informado." 
+     *  }
+     */
+    public function test_3(): void
+    {
+        $url = '/bem-estar';
+
+        $body = [
+            'funcionario_id' => 1,
+            'bem-estar' => 1,
+        ];
+
+        $response = $this->post(
+            $url,
+            $body
+        );
+        
+
+        assertIsString($response->json()['dica']);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Resgata o qrcode do funcionario.
+     * 
+     * GET /resposta
+     *  {
+     *      "funcionario_id" : 1,
+     *  }
+     * 
+     * 201
+     */
+    public function test_4(): void
+    {
+        $url = '/qrcode?funcionario_id=1';
+
+        $response = $this->get(
+            $url
+        );
+    
+        $response->assertStatus(201);
+    }
+
+    /**
+     * Cria o qrcode do funcionario
+     * 
+     * POST /resposta
+     *  {
+     *      "funcionario_id" : 1,
+     *  }
+     * 
+     *  200
+     */
+    public function test_5(): void
+    {
+        $url = '/qrcode';
+
+        $body = [
+            'funcionario_id' => 1
+        ];
+
+        $response = $this->post(
+            $url,
+            $body
+        );
+    
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Cria as dicas para o funcionario
+     * 
+     * POST /resposta
+     *  {
+     *      "funcionario_id" : 1,
+     *  }
+     * 
+     * 200
+     */
+    public function test_6(): void
+    {
+        $url = '/dica';
+
+        $body = [
+            'funcionario-id' => 1,
+            'bem-estar' => 1,
+            'dica' => 'Tire um day-off, o seu gestor já foi informado.'
+        ];
+
+        $response = $this->post(
+            $url,
+            $body
+        );
+    
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Cria as dicas para o funcionario
+     * 
+     * GET /dica?funcionario-id=1&bem-estar=1
+     *  {
+     *      "funcionario_id" : 1,
+     *  }
+     * 
+     * 201
+     */
+    public function test_7(): void
+    {
+        $url = '/dica?funcionario-id=1&bem-estar=1';
+
+        $response = $this->get(
+            $url
+        );
+    
+        $response->assertStatus(200);
+    }
+}
